1f7950f437b78ced706073d327f883674c6af463
TheBear400 <74928382+TheBear400@users.noreply.github.com>
2024-05-27 15:38:58 +0300

    Update HelloWorld.ino

--- HelloWorld.ino
+++ HelloWorld.ino
@@ -1,40 +1,50 @@
 int relay_1 = 5;
+int MQ2pin = A0;
+int sensorValue;
+int MQ2Thres = 400;
 #define Sensor 7
-#define LED 4
-#include <Wire.h> 
+#include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(0x27,16,2);
-
 void setup() {
-  lcd.init();                    
-  lcd.backlight();
-  Serial.begin(9600);
-  pinMode(Sensor, INPUT);
-  pinMode(LED, OUTPUT);
-  pinMode(relay_1, OUTPUT);
-
+lcd.init();
+lcd.init();
+lcd.backlight();
+Serial.begin(9600);
+pinMode(Sensor, INPUT);
+pinMode(relay_1, OUTPUT);
 }
-
 void loop() {
-  bool value = digitalRead(Sensor);
-  Serial.println(value);
-
-  if (value == 0) {
-    lcd.clear(); 
-    digitalWrite(LED, HIGH);
-    lcd.setCursor(1,0);
-    lcd.print("Fire Fire");
-    delay(250);
-    digitalWrite(relay_1, HIGH);
-    delay(1000);
-    digitalWrite(relay_1, LOW);
-    delay(1000);
-  } 
-    else {
-    lcd.clear();
-    digitalWrite(LED, LOW);
-    lcd.setCursor(2,0);
-    lcd.print("No Fire");
-    delay(250);
-  }
+bool value = digitalRead(Sensor);
+Serial.println(value);
+sensorValue = analogRead(MQ2pin);
+lcd.setCursor(0,0);
+lcd.print(" Value ");
+lcd.print(sensorValue);
+lcd.print("ppm");
+Serial.println(sensorValue, DEC);
+if (sensorValue > 450){
+delay(250);
+digitalWrite(relay_1, HIGH);
+delay(1000);
+digitalWrite(relay_1, LOW);
+delay(1000);
+lcd.setCursor(0,1);
+lcd.print("Smoke detected ");
+}
+else{
+lcd.setCursor(0,1);
+lcd.print("No Fire,No Gas");
+}
+if (value == 0) {
+lcd.clear();
+lcd.setCursor(0,0);
+lcd.print(sensorValue);
+lcd.print(" No Fire and No Gas");
+delay(250);
+digitalWrite(relay_1, HIGH);
+delay(1000);
+digitalWrite(relay_1, LOW);
+delay(1000);
+}
 }
